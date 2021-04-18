48372cc64a44180c95dd69b76e0446c779b81525
harris-chris <chrisharriscjh@gmail.com>
2021-04-18 20:27:48 +0900

    Remove image, scripts

--- scripts/run_image_locally.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker run \
-  -p 9000:8080 \
-  -it 111111111111.dkr.ecr.ap-northeast-1.amazonaws.com/julia-lambda:latest
--- scripts/test_image_locally.sh
+++
@@ -1,7 +0,0 @@
-if [ $# -eq 0 ]
-  then
-    echo "Please supply an argument to use as the invocation payload"
-fi
-curl -XPOST \
-  "http://localhost:9000/2015-03-31/functions/function/invocations" \
-  -d "$1"
